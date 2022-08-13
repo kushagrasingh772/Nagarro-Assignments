9522b6b322890435063eea55221f91f33ffb3eb4
Kushagra Singh <52888710+kushagrasingh772@users.noreply.github.com>
2022-08-14 00:38:10 +0530

    Add files via upload

---
+++ Assignment-10/index.js
@@ -0,0 +1,9 @@
+let count = 1;
+setInterval(()=>{
+    document.getElementById("btn" + count).checked = true;
+    document.body.style.transition = "1s ease-in";
+    document.body.style.background = `url('Images/bg-image${count}.jpg') center center / cover`;
+    document.body.style.transition = "2s ease-out";
+    count++;
+    if (count>4) count = 1
+}, 5000);
